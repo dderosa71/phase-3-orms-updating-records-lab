885de82da37b425749005932afe016be83ef30d5
dominicderosa <dderosa71@gmail.com>
2021-10-18 19:06:39 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -51,8 +51,9 @@ class Student
     SELECT * FROM students
     WHERE name = ?
     SQL
-    row = DB[:conn].execute(sql, name)
-    self.new_from_db(row[0])
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+    end.first
   end
 
   def update
